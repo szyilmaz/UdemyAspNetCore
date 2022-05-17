44593e4914719e2808697660825b1b5a10a73237
szyilmaz <szyilmaz@yukselproje.com.tr>
2022-05-17 17:43:28 +0300

    Proje dosyası ekle.

---
+++ UdemyAspNetCore/Middlewares/RequestEditingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyAspNetCore.Middlewares
+{
+    public class RequestEditingMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+
+        public RequestEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.ToString() == "/yavuz")
+                context.Response.WriteAsync("hello yavuz");
+            else
+                await _requestDelegate.Invoke(context);
+        }
+
+    }
+}
